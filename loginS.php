afdf3d12afefc9e996e00baaf49dac0dad0155f7
PIXELOZIUM\Wojtek <empox@o2.pl>
2017-11-18 09:55:33 +0100

    drobne poprawki

--- loginS.php
+++ loginS.php
@@ -34,43 +34,9 @@ session_start();
 <div class="main">
   <div class="sidebar_spacer">
   <div class="section sidebar">
-    <?php
-     
 
-
-	if ($_SESSION['log']){
-	 echo '<ul class="menu">
-        <li class="first selected"><a href="index.php">Strona główna</a></li>
-        <li><a href="plyty.php">Płyty</a></li>
-		<li><a href="albumy.php">Albumy</a></li>
-        <li><a href="wykonawcy.php">Wykonawcy</a></li>
-		<li><a href="zestawienia.php">Zestawienia</a></li>
-		<li><a href="slowniki.php">Słowniki</a></li>
-        <li><a href="szukaj.php">Szukaj</a></li>
-        <li><a href="wyloguj.php">Wyloguj</a></li>
-        <!--<li class="first"><a href="#">Bibendum quist</a></li>-->
-      </ul>
-      <div id="sidebar_dec"></div>
-    <div class="clear"></div>
-  </div>
-  </div>';
-  }
-  else{
-  echo '<ul class="menu">
-        <li class="first selected"><a href="index.php">Strona główna</a></li>
-        <li><a href="plyty.php">Płyty</a></li>
-		<li><a href="albumy.php">Albumy</a></li>
-        <li><a href="wykonawcy.php">Wykonawcy</a></li>
-		<li><a href="zestawienia.php">Zestawienia</a></li>
-        <li><a href="szukaj.php">Szukaj</a></li>
-        <li><a href="login.php">Zaloguj</a></li>
-        <!--<li class="first"><a href="#">Bibendum quist</a></li>-->
-      </ul>
-      <div id="sidebar_dec"></div>
-    <div class="clear"></div>
-  </div>
-  </div>';
-  }
+  <?php
+    include 'menu.php';
   ?>
   
   
